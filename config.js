1c5ffb49df5a94a27777bb99f3d3dc1f9e4ab54e
dilazz16 <dilmika00@gmail.com>
2026-06-06 00:32:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/dilazz16/Dilazz/blob/main/images/IMG_1116.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DILAZZ-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94702825306',  // Replace with the owner's phone number
 
 
 
